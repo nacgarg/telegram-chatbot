c6b086f143cf3738c1232153ce4390b2b2d8ba68
Aidan Lloyd-Tucker <aidanlloydtucker@gmail.com>
2015-11-24 09:17:02 -0800

    a better bot

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
 require('dotenv').load();
 
-var telegram = require('telegram-bot-api');
+var TelegramBot = require('node-telegram-bot-api');
 var markov = require('./markov');
 
-var api = new telegram({
-    token: process.env.TELEGRAM_TOKEN,
-    updates: {
-        enabled: true
+
+var bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {
+    polling: {
+        interval: 1,
+        timeout: 4
     }
+
 });
 
-api.on('message', function(message) {
-    console.log(message);
-    if (message.text) {
-    	message.text = message.text.toLowerCase();
-        if (message.text == '/data') {
-		if(message.from.first_name=="Lurf"){
-		api.sendMessage({chat_id:message.chat.id,text:"lurf you are gibbon, not scrub"});
-		return;
-		}
-if(message.from.first_name=="Nachi"){
-                api.sendMessage({chat_id:message.chat.id,text:"fibbonachiketa you are TOTAL scrub"});
-                return;
-                }
-            api.sendMessage({
-                chat_id: message.chat.id,
-                text: "Nice try, " + message.from.first_name + ", you filthy skrub."
-            })
-        } else if (message.text.slice(0, 6) === 'markov') {
-            api.sendMessage({
-                chat_id: message.chat.id,
-                text: markov.respond(message.text.substring(6))
-            });
-        } else {
-            markov.learn(message.text);
-        }
+bot.onText(/^\/markov(?:@automod_bot)? (.*)$/, function (msg, match) {
+    if (!canRecieve(msg.from)) {
+        return;
     }
+    console.log("recieved");
+    var fromId = msg.chat.id;
+    bot.sendMessage(fromId, markov.respond(match[1]), {
+        parse_mode: 'markdown'
+    });
+});
 
+bot.on('message', function (msg) {
+    var fromId = msg.chat.id;
+    if (msg.text && msg.indexOf('/markov') === -1) {
+        markov.learn(msg.text);
+    }
 });
